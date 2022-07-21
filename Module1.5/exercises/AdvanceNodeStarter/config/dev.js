d5bcaa982e948279161aa47bbf5a99d31bea0df0
luiscg98 <luis.grimaldo@unosquare.com>
2022-07-21 02:38:59 -0500

    Module1.5: install redis

---
+++ Module1.5/exercises/AdvanceNodeStarter/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://chagri98:chagri7biomedica@cluster0-shard-00-00.tjgvd.mongodb.net:27017,cluster0-shard-00-01.tjgvd.mongodb.net:27017,cluster0-shard-00-02.tjgvd.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-etqtnz-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
